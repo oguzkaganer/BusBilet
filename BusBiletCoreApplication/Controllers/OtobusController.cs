88ef9fb368557946d91039fca7146fffb241926c
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-12-26 23:44:39 +0300

    Added OtobusController.cs

---
+++ BusBiletCoreApplication/Controllers/OtobusController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusBiletCoreApplication.Controllers
+{
+    public class OtobusController : Controller
+    {
+        IOtobusService _otobusService;
+
+        public OtobusController(IOtobusService otobusService)
+        {
+            _otobusService = otobusService;
+        }
+
+        public IActionResult Index()
+        {
+            var otobusler = _otobusService.otobusListele();
+            return View(otobusler);
+        }
+    }
+}
