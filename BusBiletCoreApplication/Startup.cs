8b180e22907803ff16aba3aeb37bbb175c20fac0
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-12-28 23:07:02 +0300

    Updated Startup.cs

--- BusBiletCoreApplication/Startup.cs
+++ BusBiletCoreApplication/Startup.cs
@@ -59,7 +59,7 @@ namespace BusBiletCoreApplication
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Firma}/{action=Index}");
             });
         }
     }
